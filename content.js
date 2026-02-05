f6e43ce2c6dd487fe3c5322079d48b020c0b0a99
sooryakabir <135860915+sooryakabir@users.noreply.github.com>
2026-02-05 21:19:34 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,13 @@
+function apply(enabled) {
+  document.documentElement.setAttribute("data-accessible", enabled ? "true" : "false");
+}
+
+// Apply saved setting on page load
+chrome.storage.sync.get(["enabled"], (res) => {
+  apply(!!res.enabled);
+});
+
+// Listen for popup toggle messages
+chrome.runtime.onMessage.addListener((msg) => {
+  if (msg.type === "SET_ACCESSIBLE") apply(!!msg.enabled);
+});
